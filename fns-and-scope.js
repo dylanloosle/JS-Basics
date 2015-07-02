c8289cbbd98465f873f3f72970d64fc53ca2c5f1
Dylan Loosle <dylanloosle@gmail.com>
2015-07-01 20:36:53 -0600

    first submit

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -1,11 +1,21 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-var name = 'Tyler';
+
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+var name = 'Tyler';
+var isTyler = function(){
+    if(name === 'Tyler') {
+        return 'true';
+    }
+    else {
+        return 'false';
+    }
+};
+isTyler();
 
 
 //Next problem
@@ -16,7 +26,11 @@ var name = 'Tyler';
 
 
   //Code Here
-
+var getName = function(){
+    var userName = prompt('What is your name?');
+    return userName;
+};
+getName();
 
 //Next Problem
 
@@ -26,7 +40,10 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+var welcome = function(){
+    alert("Welcome," + userName);
+}
+welcome();
 
 //Next problem
 
@@ -36,6 +53,8 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
+/* parameters are inside the paranthesis next to function and are typically variables defined within a function*/
+/* arguments are actual values used when CALLING the function that correspond with the parameters set earlier */
 
 
 //Next problem
@@ -46,7 +65,23 @@ var name = 'Tyler';
 
 
   //Answer Here
+/*/
+ emptry string
+ 0
+ false
+ undefined
+ null
+ NaN
 
+ To check if something is falsy, you can just test the function with an if else statement. EXAMPLE:
+ if(foo) {
+ return true;
+
+ else {
+ return false;
+ }
+ }
+ */
 
 
 //Next Problem
@@ -56,13 +91,18 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
+var myName = function(){
+    return 'Dylan';
+};
+myName();
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
+var newMyName = myName;
 
 //Now alert the result of invoking newMyName
-
+alert(newMyName);
 
 
 //Next problem
@@ -72,9 +112,14 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+var outerFn = function(){
+    return 'Dylan'
+};
+
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+console.log(innerFn);
